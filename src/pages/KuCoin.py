491bbca9155292940e2e76751d119563bc4b7314
thiendsu2303 <vithien23@gmail.com>
2023-04-12 01:15:24 +0700

    KuCoin test

--- src/pages/KuCoin.py
+++ src/pages/KuCoin.py
@@ -1,16 +1,4 @@
 import streamlit as st
-import datetime
-import pandas as pd
-import numpy as np
-import ProcessingData as tt
-
-apiKey = "PSI4IFEFAUF8USVB6M5BSEAM1VB9CX7FJZ"
-data = pd.read_csv('./input.csv')
-test_address = data['address'].values
-address = test_address[0]
-page = 1
-offset = 10000
-list = tt.ProcessingData(address, page, offset,apiKey)
 
 st.title("Dữ liệu của sàn :red[KuCoin]:chart_with_upwards_trend:")
 
